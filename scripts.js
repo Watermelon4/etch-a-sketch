01a4f3eb676c5664c840b6bcac8099c1b7bbe540
George Mao <zhongqilin@gmail.com>
2022-05-05 20:23:25 -0400

    Create outline for needed functions

---
+++ scripts.js
@@ -0,0 +1,49 @@
+/* TODO
+- generate x boxes based on the size of the grid and append to grid
+- select boxes based on mouse hover
+- edit color of boxes when clicked or held
+- color select
+- reset
+- edit grid size
+
+createGrid(length) {
+  for gridlength**2 {
+  create box[i]
+  append box[i]
+  }
+}
+
+eventlistener(hover) {
+  get box
+}
+
+eventlistener(click/hold) {
+  changeColor(box, color)
+}
+
+changeBoxColor(box, color) {
+  box.backGround = color
+}
+
+reset() {
+  for numBoxes {
+    box[numBox].color = white
+  }
+}
+
+editGridSize(length) {
+  deleteGrid()
+}
+
+deleteGrid() {
+  delete whole grid?
+  or
+  for numBoxes {
+    del box
+  }
+}
+
+getSelectedColor(input) {
+  selected = input
+}
+*/
