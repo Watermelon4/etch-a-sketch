a1b3f0a32c269ee0f379e78e69ff2f5f58a65e25
George Mao <zhongqilin@gmail.com>
2022-05-05 20:24:35 -0400

    Add function to generate a grid based on box size

--- scripts.js
+++ scripts.js
@@ -47,3 +47,28 @@ getSelectedColor(input) {
   selected = input
 }
 */
+
+const main = document.querySelector("main")
+const canvas = main.querySelector(".canvas");
+let grid = document.createElement("div");
+
+grid.style.gridTemplate = "repeat(8, 64px) / repeat(8, 64px)";
+canvas.appendChild(grid);
+let box = document.createElement("div");
+grid.appendChild(box);
+let a = 0;
+
+function createGrid(gridSize) {
+  gridSize = Number(gridSize);
+  grid.setAttribute("class", "grid");
+  const gridStyles = getComputedStyle(grid)
+  const gridWidth = Number(gridStyles.width);
+
+  const boxWidth = gridWidth / gridSize;
+  let gridTemplateStyle = `return(${gridSize}, ${boxWidth}) / 
+                           return(${gridSize}, ${boxWidth})`;
+  grid.style.gridTemplate = gridTemplateStyle;
+}
+
+let gridSize = "8";
+createGrid(gridSize);
