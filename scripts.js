c7478422d685a165e53d6d1e06b26560ad9a84a0
George Mao <zhongqilin@gmail.com>
2022-05-06 19:53:05 -0400

    Create function to set up basic events for boxes
    - color boxes, (no choice)
    - border on box hovered

--- scripts.js
+++ scripts.js
@@ -41,7 +41,7 @@ getSelectedColor(input) {
 }
 */
 
-let gridSizeInput = "64"; // simulate selection
+let gridSizeInput = "16"; // simulate selection
 let gridSize = Number(gridSizeInput); 
 let gridWidthString = "512";
 let gridWidth = Number(gridWidthString); // simulate
@@ -65,8 +65,22 @@ function populateGrid() {
   for (let i = 0; i < numBoxesToCreate; i++) {
     let gridBoxElement = document.createElement("div");
     gridBoxElement.setAttribute("id", `box-${i}`)
+    gridBoxElement = setGridBoxEventListeners(gridBoxElement);
     gridElement.append(gridBoxElement);
   }
+}
+
+function setGridBoxEventListeners(gridBoxElement) {
+  gridBoxElement.addEventListener("click", function() {
+    gridBoxElement.style.backgroundColor = "blue";
+  });
+  gridBoxElement.addEventListener("mouseover", function() {
+    gridBoxElement.style.border = "1px dashed grey"
+  });
+  gridBoxElement.addEventListener("mouseout", function() {
+    gridBoxElement.style.border = ""
+  });
+  return gridBoxElement
 }
 
 function resetGrid() {
